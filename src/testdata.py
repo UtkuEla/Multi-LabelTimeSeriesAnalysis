9ad7eea2e5fe3c632bdbb402880ea0fbc693a8bb
Utku <elagozutku@gmail.com>
2023-07-07 00:00:30 +0200

    data classes

--- src/testdata.py
+++ src/testdata.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 
-class trainData():
+class testData():
 
-    def __init__(self, sampleSize, overlapRatio):
+    def __init__(self):
 
         self.current_folder_path = os.getcwd()
         self.parent_folder_path = os.path.dirname(self.current_folder_path)
-        self.data_save_path = os.path.join(self.parent_folder_path, 'train_data')
+        self.data_save_path = os.path.join(self.parent_folder_path, 'test_data')
+
 
     def addDataPathtoCurrent(self,path):
         return os.path.join(self.current_folder_path,path)
@@ -16,17 +17,22 @@ class trainData():
     def readData(self,string):
 
         df = pd.read_feather(string)
+
         if "time" in df.columns :
             df = df.drop(columns = {'time'})
 
-        df = df.drop(index=0)
+        self.df = df.drop(index=0)
 
-    def splitData(self,df):
-
+    def splitData(self):
+        df = self.df
         df['label'] = df['label'].str.replace(' ', '')
 
         # Split the dataset based on labels
-        labels = ['Bias', 'Drift', 'Gain', 'NoFault', 'Outliers', 'Precisiondegradation']
+        labels = ['Bias+Drift', 'Bias+Gain', 'Bias+Precisiondegradation', 'Bias',
+       'Bias+Outliers', 'Drift+Gain', 'Drift+Precisiondegradation',
+       'Drift', 'Drift+Outliers', 'Gain+Precisiondegradation', 'Gain',
+       'Gain+Outliers', 'Precisiondegradation',
+       'Precisiondegradation+Outliers']
 
         self.datasets = {}
         self.dataframes =[]
@@ -38,17 +44,6 @@ class trainData():
             globals()[f'df_{label}'] = self.datasets[label]
             self.dataframes.append(f'df_{label}')
             #datasets[label].to_csv(self.data_save_path + "/" +f'df_{label}.csv', index=False)
-
-        try:
-            print("Size of df_Bias:", df_Bias.shape)
-            print("Size of df_Drift:", df_Drift.shape)
-            print("Size of df_Gain:", df_Gain.shape)
-            print("Size of df_NoFault:", df_NoFault.shape)
-            print("Size of df_Outliers:", df_Outliers.shape)
-            print("Size of df_PrecisionDegredatation:", df_Precisiondegradation.shape)
-
-        except:
-            pass
 
     def split_column_into_rows(self, df, num_columns, overlapping_ratio):
         
@@ -77,39 +72,39 @@ class trainData():
 
         return new_df
     
-    def mergeData(self):
-        self.train_data = pd.DataFrame()
+    def mergeData(self, sampleSize, overlapRatio):
+        self.test_data = pd.DataFrame()
         
         for key in self.datasets.keys():
 
             df = self.datasets[key]
             df = df.reset_index(drop=True, inplace=False)
-            df = self.split_column_into_rows(df,self.sampleSize,self.overlapRatio)
+            df = self.split_column_into_rows(df,sampleSize, overlapRatio)
             df = df.drop(df.index[-1])
-            self.train_data = pd.concat([self.train_data,df])
+            self.test_data = pd.concat([self.test_data,df])
 
-        self.train_data = self.train_data.reset_index(drop=True, inplace=False)
+        self.test_data = self.test_data.reset_index(drop=True, inplace=False)
 
-        print('test data shape: ' + self.train_data.shape)
+        print('test data shape: ' + self.test_data.shape)
 
     def prepareOutput(self):
         
-        train_labels = self.train_data['label'].values
-        train_data_values = self.train_data.drop(columns={'label'})
+        test_labels = self.test_data['label'].values
+        test_data_values = self.test_data.drop(columns={'label'})
 
-        trainName = 'train_data_values_' + str(self.sampleSize) + "_" + str(self.overlapRatio)
-        labelName = 'train_labels_' + str(self.sampleSize) + "_" + str(self.overlapRatio)
+        testName = 'test_data_values_' + str(self.sampleSize) + "_" + str(self.overlapRatio)
+        labelName = 'test_labels_' + str(self.sampleSize) + "_" + str(self.overlapRatio)
         
-        train_data_values.to_pickle(trainName)
-        train_labels = pd.DataFrame(train_labels)
-        train_labels.to_pickle(labelName)
+        test_data_values.to_pickle(testName)
+        test_labels = pd.DataFrame(test_labels)
+        test_labels.to_pickle(labelName)
 
-        print(len(train_data_values))
-        print(train_labels.shape)
+        print(len(test_data_values))
+        print(test_labels.shape)
 
-        return train_data_values, train_labels
+        return test_data_values, test_labels
     
-    def preparetrainData(self, sampleSize, overlapRatio, path):
+    def prepareTestData(self, sampleSize, overlapRatio, path):
         self.sampleSize = sampleSize
         self.overlapRatio = overlapRatio
         self.feather_path = os.path.join(self.data_save_path, path)
@@ -117,3 +112,4 @@ class trainData():
         self.splitData()
         self.mergeData(sampleSize, overlapRatio)
         self.prepareOutput()
+
